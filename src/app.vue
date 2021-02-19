34df48d55fa29f372346490d78babbd335d7bc0f
helan <helan@hongshantech.com.cn>
2021-02-19 15:42:24 +0800

    first

---
+++ src/app.vue
@@ -0,0 +1,19 @@
+<template>
+    <div id="test">{{test}}</div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            test:'vueDemo'
+        }
+    }
+}
+</script>
+
+<style>
+#test{
+    color: red;
+}
+</style>
